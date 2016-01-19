edb2f0f1554b942d58e310fb83f9bea4b88a8581
Menuka Samaranayake <menuka.samaranayake@gmail.com>
2016-01-19 13:57:26 -0600

    Complete 7.2 challenge

---
+++ week-7/eloquent.js
@@ -0,0 +1,59 @@
+// Eloquent JavaScript
+
+// Run this file in your terminal using `node my_solution.js`. Make sure it works before moving on!
+
+// Program Structure
+// Write your own variable and do something to it.
+
+/*
+var my_name = "Menuka";
+//console.log(my_name);
+
+prompt("What's your favorite food");
+console.log("Hey! That's my favorite too!");
+*/
+
+// Complete one of the exercises: Looping a Triangle, FizzBuzz, or Chess Board
+
+
+/*
+for (var count = 1; count <= 100; count += 1) {
+	if (count % 3 == 0 && count % 5 == 0) {
+		console.log("FizzBuzz");
+	} else if (count % 3 == 0) {
+		console.log("Fizz");
+	} else if (count % 5 == 0) {
+		console.log("Buzz");
+	} else {
+		console.log(count);
+	}
+}
+*/
+
+
+
+// Functions
+
+// Complete the `minimum` exercise.
+
+/*
+
+function min (a, b){
+	return Math.min(a, b);
+}
+
+*/
+
+
+// Data Structures: Objects and Arrays
+// Create an object called "me" that stores your name, age, 3 favorite foods, and a quirk below.
+
+var me = {
+	name: "Menuka",
+	age: 24,
+	fav_foods: ["pizza","steak","wings"],
+	quirk: "Full name contains 35 letters"
+
+}
+
+//console.log(me.name)
